6b102c75034cc0bfb609655c32fb3a97da55fe77
howshecodes <102827973+howshecodes@users.noreply.github.com>
2022-07-26 19:22:02 -0400

    usereducer assignment

---
+++ useReducerHookConcept-6363dd48d5cc93f5255d817d07df25f3c071630c/src/App.js
@@ -0,0 +1,25 @@
+// useReducer: simple Counter
+
+import * as React from "react";
+
+function Counter({ initialCount = 0, step = 1 }) {
+  // 🐨 replace React.useState with React.useReducer.
+  // 💰 React.useReducer(countReducer, initialCount)
+  function countReducer(state, newState) {
+    return newState;
+  }
+  const [count, setCount] = React.useReducer(countReducer, initialCount);
+
+  // 💰 you can write the countReducer function so you don't have to make any
+  // changes to the next two lines of code! Remember:
+  // The 1st argument is called "state" - the current value of count
+  // The 2nd argument is called "newState" - the value passed to setCount
+  const increment = () => setCount(count + step);
+  return <button onClick={increment}>{count}</button>;
+}
+
+function App() {
+  return <Counter />;
+}
+
+export default App;
